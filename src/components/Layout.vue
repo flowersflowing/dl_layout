66201657761e5a1a386442b681c71f2d01799dbd
Natalia <nflores.teoria@gmail.com>
2020-12-16 20:54:20 -0300

    Agregando navbar

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,20 +1,53 @@
 <template>
-  <div class="layout">
+  <div>
+    <b-navbar toggleable type="light" variant="light">
+      <b-navbar-brand href="#">Mi App</b-navbar-brand>
+
+      <div class="items">
+        <b-nav-item>INICIO</b-nav-item>
+        <b-nav-item>BUSCAR</b-nav-item>
+        <b-nav-item>ACERCA DE</b-nav-item> 
+      </div>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Layout',
-  data() {
-    pers: [
-      
-    ]
-  }
+  name: 'Layout'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import 'node_modules/bootstrap/scss/bootstrap.scss';
+.items {
+  list-style: none;
+  display: flex;
+  justify-content: end;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@ import Layout from '../components/Layout.vue';
 export default {
   name: 'Home',
   components: {
-    Layout,
+    Layout
   }
 }
 </script>
