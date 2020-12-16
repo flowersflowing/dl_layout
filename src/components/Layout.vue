29b15439e3ba4bf8d1fb1acdc7442e3dfeaaf764
Natalia <nflores.teoria@gmail.com>
2020-12-16 20:59:33 -0300

    Agregando cards todavía no formateada

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -34,6 +34,15 @@
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
+
+    <b-card-group>
+      <b-card title="Title" img-src="https://placekitten.com/g/300/450" img-alt="Image" img-top>
+        <b-card-text>
+          This is a wider card with supporting text below as a natural lead-in to additional content.
+          This content is a little bit longer.
+        </b-card-text>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
